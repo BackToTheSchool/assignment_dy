51438d78f635f3c3e8dfa743615f8f4952b49edb
Jeonous <corea940@gmail.com>
2017-10-26 00:16:05 +0900

    Update 열혈C(3-1-1).c

--- 열혈C(3-1-1).c
+++ 열혈C(3-1-1).c
@@ -6,14 +6,14 @@ int main()
 	int result1, result2;
 	int num1, num2;
 
-	printf("두 개의 정수를 입력하시오. \n");
+	printf(" \n");
 	scanf_s("%d %d", &num1, &num2);
 	
 	result1 = num1 * num2;
 	result2 = num1 - num2;
 
-	printf("두 수의 곱은 %d입니다. \n", result1);
-	printf("두 수의 차는 %d입니다.", result2);
+	printf(". \n", result1);
+	printf(".", result2);
 
 	system("pause");
 	
